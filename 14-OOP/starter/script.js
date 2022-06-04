7197b8dc1228dc4cda72248ebd9230cbdf0ea3d1
yoneda <chat.414.kome@gmail.com>
2022-06-04 12:02:42 +0900

    Coding Challenge

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,62 +1,99 @@
-'use strict';
-//allow function not work with constructor because we need keyword and allow function doesn't have that.
+// 'use strict';
+// //allow function not work with constructor because we need keyword and allow function doesn't have that.
 
-//constructor like blueprint of house
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// //constructor like blueprint of house
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  //Never to this. don't use method inside constructor because if we create thousand of instance, for each instance this function will work and that's terrible for performance.
-  //   this.calcAge = function () {
-  //     console.log(2037 - this.birthYear);
-  //   };
+//   //Never to this. don't use method inside constructor because if we create thousand of instance, for each instance this function will work and that's terrible for performance.
+//   //   this.calcAge = function () {
+//   //     console.log(2037 - this.birthYear);
+//   //   };
+// };
+
+// //new operator
+// const jonas = new Person('Jonas', 1991);
+// console.log(jonas);
+
+// console.log(jonas instanceof Person);
+
+// // 1. New {} is created
+// // 2. function is called, this = {}
+// // 3. {} linked to prototype
+// // 4. function automatically return {}
+
+// //Prototypes
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+// jonas.calcAge();
+// //Each object created by this construction will now get access to all the method of this prototype method.
+
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
+
+// console.log(Person.prototype.isPrototypeOf(jonas));
+
+// Person.prototype.species = 'Homo sapiens';
+// console.log(jonas.species);
+// console.log(jonas.hasOwnProperty('firstName'));
+// console.log(jonas.hasOwnProperty('species'));
+// //species is just be able to access from jonas. not property inside.
+
+// //Object.prototype (top of prototype of chain)
+// console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__.__proto__.__proto__);
+
+// console.log(Person.prototype.constructor);
+
+// const arr = [3, 6, 4, 5, 6, 9, 3];
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+// //arr.__proto__ is object
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(x => x + 1);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-//new operator
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+console.log(bmw);
 
-console.log(jonas instanceof Person);
-
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically return {}
-
-//Prototypes
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
-jonas.calcAge();
-//Each object created by this construction will now get access to all the method of this prototype method.
-
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
-
-console.log(Person.prototype.isPrototypeOf(jonas));
-
-Person.prototype.species = 'Homo sapiens';
-console.log(jonas.species);
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
-//species is just be able to access from jonas. not property inside.
-
-//Object.prototype (top of prototype of chain)
-console.log(jonas.__proto__.__proto__);
-console.log(jonas.__proto__.__proto__.__proto__);
-
-console.log(Person.prototype.constructor);
-
-const arr = [3, 6, 4, 5, 6, 9, 3];
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
-//arr.__proto__ is object
-
-Array.prototype.unique = function () {
-  return [...new Set(this)];
+Car.prototype.accelerate = function (speed) {
+  console.log((this.speed += 10));
 };
 
-console.log(arr.unique());
+Car.prototype.brake = function (speed) {
+  console.log((this.speed -= 5));
+};
 
-const h1 = document.querySelector('h1');
-console.dir(x => x + 1);
+bmw.accelerate();
+bmw.brake();
+
+//OOP Pack data and function in obj
