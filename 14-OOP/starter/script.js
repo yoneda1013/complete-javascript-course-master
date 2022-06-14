643561c1545e0b41f0195522c12823ed946f63eb
yoneda <chat.414.kome@gmail.com>
2022-06-14 20:06:38 +0900

    217. Coding Challenge #2

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -82,26 +82,62 @@ DATA CAR 2: 'Mercedes' going at 95 km/h
 GOOD LUCK 😀
 */
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-const bmw = new Car('BMW', 120);
-const mercedes = new Car('Mercedes', 95);
-console.log(bmw);
+// const bmw = new Car('BMW', 120);
+// const mercedes = new Car('Mercedes', 95);
+// console.log(bmw);
 
-Car.prototype.accelerate = function (speed) {
-  console.log((this.speed += 10));
-};
+// Car.prototype.accelerate = function (speed) {
+//   console.log((this.speed += 10));
+// };
 
-Car.prototype.brake = function (speed) {
-  console.log((this.speed -= 5));
-};
+// Car.prototype.brake = function (speed) {
+//   console.log((this.speed -= 5));
+// };
 
-bmw.accelerate();
-bmw.brake();
+// bmw.accelerate();
+// bmw.brake();
 
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+GOOD LUCK 😀
+*/
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate = function (speed) {
+    console.log((this.speed += 10));
+  };
+
+  brake = function (speed) {
+    console.log((this.speed -= 5));
+  };
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    return this.speed * 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
 //OOP Pack data and function in obj
 
 ///////////////////////////////////////
