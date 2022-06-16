fa99036f3b9793eb35c98db54d45f217cd80027b
yoneda <chat.414.kome@gmail.com>
2022-06-17 08:33:37 +0900

    222 Inheritance between "Classes" Object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -309,93 +309,123 @@ GOOD LUCK 😀
 ///////////////////////////////////////
 // Inheritance between "Classes": Constructor functions
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 
-const Student = function (firstName, birthYear, course) {
-  Person.call(this, firstName, birthYear);
-  this.course = course;
-};
+// const Student = function (firstName, birthYear, course) {
+//   Person.call(this, firstName, birthYear);
+//   this.course = course;
+// };
 
-//Linking prototypes
-Student.prototype = Object.create(Person.prototype);
+// //Linking prototypes
+// Student.prototype = Object.create(Person.prototype);
 
-Student.prototype.introduce = function () {
-  console.log(`My name is ${this.firstName} and I study ${this.course}`);
-};
+// Student.prototype.introduce = function () {
+//   console.log(`My name is ${this.firstName} and I study ${this.course}`);
+// };
 
-const mike = new Student('Mike', 2020, 'Computer Science');
-mike.introduce();
-mike.calcAge();
+// const mike = new Student('Mike', 2020, 'Computer Science');
+// mike.introduce();
+// mike.calcAge();
 
-console.log(mike instanceof Student);
-console.log(mike instanceof Person);
+// console.log(mike instanceof Student);
+// console.log(mike instanceof Person);
 
-console.log(mike.__proto__.__proto__);
+// console.log(mike.__proto__.__proto__);
 
 ///////////////////////////////////////
 // Inheritance between "Classes" ES6
 
-class PersonCl {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
 
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
+
+//   greet() {
+//     console.log(`Hey ${this.firstName}`);
+//   }
+
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
+
+//   set fullName(name) {
+//     if (name.includes(' ')) this._fullName = name;
+//   }
+
+//   get fullName() {
+//     return this._fullName;
+//   }
+
+//   static hey() {
+//     console.log('Hey there✋');
+//   }
+// }
+
+// class StudentCl extends PersonCl {
+//   constructor(fullName, birthYear, course) {
+//     //Always needs to happen first
+//     super(fullName, birthYear);
+//     this.course = course;
+//     //similar constructor function
+//   }
+//   introduce() {
+//     console.log(`My name is ${this.fullName} and I study ${this.course}`);
+//   }
+//   //プロトタイプチェーンの一番最初にくるので、後にくるparentのfunctionをoverrideする
+//   calcAge() {
+//     console.log(
+//       `I'm ${
+//         2037 - this.birthYear
+//       } years old, but as a student I feel more like ${
+//         2037 - this.birthYear + 10
+//       }`
+//     );
+//   }
+// }
+
+// const martha = new StudentCl('Martha Jones', 2022, 'Computer Science');
+// // const martha = new StudentCl('Martha Jones', 2022, 'Computer Science');
+// martha.introduce();
+// martha.calcAge();
+
+///////////////////////////////////////
+// Inheritance between "Classes" Object.create
+
+const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
-  }
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
 
-  greet() {
-    console.log(`Hey ${this.firstName}`);
-  }
+const steven = Object.create(PersonProto);
 
-  get age() {
-    return 2037 - this.birthYear;
-  }
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
 
-  set fullName(name) {
-    if (name.includes(' ')) this._fullName = name;
-  }
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
 
-  get fullName() {
-    return this._fullName;
-  }
-
-  static hey() {
-    console.log('Hey there✋');
-  }
-}
-
-class StudentCl extends PersonCl {
-  constructor(fullName, birthYear, course) {
-    //Always needs to happen first
-    super(fullName, birthYear);
-    this.course = course;
-    //similar constructor function
-  }
-  introduce() {
-    console.log(`My name is ${this.fullName} and I study ${this.course}`);
-  }
-  //プロトタイプチェーンの一番最初にくるので、後にくるparentのfunctionをoverrideする
-  calcAge() {
-    console.log(
-      `I'm ${
-        2037 - this.birthYear
-      } years old, but as a student I feel more like ${
-        2037 - this.birthYear + 10
-      }`
-    );
-  }
-}
-
-const martha = new StudentCl('Martha Jones', 2022, 'Computer Science');
-// const martha = new StudentCl('Martha Jones', 2022, 'Computer Science');
-martha.introduce();
-martha.calcAge();
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
